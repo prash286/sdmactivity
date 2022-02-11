6dfdccf42d04e4d99ca6b92347956ff86da7a6c8
prash286 <pr.yesankar@gmail.com>
2022-02-11 09:03:34 +0530

    Issue windows authentication

---
+++ test2.java
@@ -0,0 +1,16 @@
+package authentication;
+
+import java.sql.Date;
+import java.util.LinkedList;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("**************************************************");
+		System.out.println("**************************************************");
+		System.out.println("**************************************************");
+		
+	}
+
+}
